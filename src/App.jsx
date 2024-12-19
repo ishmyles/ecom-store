c132018bb07d871fd36f7038ef3b4fef35c74ed2
Myles Gaerlan <ish.myles@gmail.com>
2024-12-19 20:04:39 +1100

    feat: refactored app file
    refactored app component to hold the routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,45 @@ import { Outlet } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer';
 
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+
+import Home from './components/HomePage/Home/Home'
+import Shop from './components/ShopPage/Shop/Shop'
+import ShoppingCart from './components/ShoppingCart/ShoppingCart'
+import ErrorPage from './components/ErrorPage/ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <>
+                <Navbar />
+                <Outlet />
+                <Footer />
+             </>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "shop/",
+        element: <Shop />,
+      },
+      {
+        path: "shop/cart",
+        element: <ShoppingCart />,
+      },
+    ],
+  },
+]);
+
 function App() {
 
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
 
 import './index.css'
 
 import App from './App'
-import Home from './components/HomePage/Home/Home'
-import Shop from './components/ShopPage/Shop/Shop'
-import ShoppingCart from './components/ShoppingCart/ShoppingCart'
-import ErrorPage from './components/ErrorPage/ErrorPage'
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "shop/",
-        element: <Shop />,
-      },
-      {
-        path: "shop/cart",
-        element: <ShoppingCart />,
-      },
-    ],
-    errorElement: <ErrorPage />
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
